cbf0d4e19f1a55f5f570f2eb63e1fc179b30fff4
eddycollotonn <eddy.colloton@gmail.com>
2025-02-21 10:04:05 -0700

    adding bottle info

--- Formula/AV-Spex.rb
+++ Formula/AV-Spex.rb
@@ -8,6 +8,12 @@ class AvSpex < Formula
   sha256 "1984677f24a4cf36a5769c4352e8dbb3ea61034e45556447fa9f52d1f9011f95"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://github.com/JPC-AV/JPC_AV_videoQC/releases/download/v0.7.1"
+    sha256 cellar: :any_skip_relocation, 
+    arm64_sonoma: "3037da13af06a5db2144da41491716cfc487b86da45a7a723faf16311e05e0e5"
+  end
+
   depends_on "python@3.10"
   depends_on "pyqt"
   depends_on "qt@6"
