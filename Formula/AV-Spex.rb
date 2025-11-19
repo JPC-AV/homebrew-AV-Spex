57dda63c8fd838dbb00c21a35557472bc2035555
GitHub Action <action@github.com>
2025-11-19 17:21:22 +0000

    Update AV-Spex to version 0.8.5.2
    - Updated URL to tag v0.8.5.2
    
    - Updated SHA256 to eda1671d928fbee71e04b52b2c98952019b553672165d375affdcd98cd279a33
    
    - Automated update from GitHub Actions

--- Formula/AV-Spex.rb
+++ Formula/AV-Spex.rb
@@ -3,8 +3,8 @@ class AvSpex < Formula
 
   desc "Python project for NMAAHC media conservation lab"
   homepage "https://github.com/JPC-AV/video_qc_jpc_av"
-  url "https://github.com/JPC-AV/video_qc_jpc_av/archive/refs/tags/v0.8.5.1.tar.gz"
-  sha256 "bc986ca5bccf1e9fce4154ec6e348693827f2b11c609d2bd552e71791aaa5d3b"
+  url "https://github.com/JPC-AV/video_qc_jpc_av/archive/refs/tags/v0.8.5.2.tar.gz"
+  sha256 "eda1671d928fbee71e04b52b2c98952019b553672165d375affdcd98cd279a33"
   license "GPL-3.0-only"
 
   depends_on "python@3.13"
